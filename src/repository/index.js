3cca573676415f315f442d6a0416e39c2d15e4cb
madhu <rayi.madhupriya@gmail.com>
2024-02-29 22:31:02 +0530

    Added city service

---
+++ src/repository/index.js
@@ -0,0 +1,4 @@
+module.exports={
+    Cityrepository: require('./city-repository')
+}
+
---
+++ src/services/city-service.js
@@ -0,0 +1,48 @@
+const { cityRepository }= require('../repository/index');
+
+class cityService{
+    constructor(){
+        this.cityRepository=new cityRepository();
+    }
+    async createCity(data){
+        try{
+            const city= await this.cityRepository.createCity(data);
+            return city;
+        }
+        catch(err){
+            console.log("something went wrong at service layer");
+            throw(err);
+        }
+    }
+    async deleteCity(cityId){
+        try{
+            const response= this.cityRepository.deleteCity(cityId);
+            return response;
+        }
+        catch(err){
+            console.log("something went wrong at service layer");
+            throw(err);
+        }
+    }
+    async updateCity(cityId,data){
+        try{
+            const city=await this.cityRepository.updateCity(cityId,data);
+            return city;
+        }
+        catch(err){
+            console.log("something went wrong at service layer");
+            throw(err);
+        }
+    }
+    async getCity(cityId){
+        try{
+            const city=await this.cityRepository.getCity(cityId);
+            return city;
+        }catch(err){
+            console.log("something went wrong at service layer");
+            throw(err);
+        }
+    }
+}
+
+module.exports = cityService;
