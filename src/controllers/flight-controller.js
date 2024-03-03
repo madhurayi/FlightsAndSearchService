389c2cf25f7c43ab30d26519261528b7a1a38fd6
madhu <rayi.madhupriya@gmail.com>
2024-03-03 12:30:53 +0530

    Added flight-create request middleware

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -4,7 +4,16 @@ const flightService= new FlightService();
 
 const create =async (req,res)=>{
     try{
-        const flight= await flightService.createFlight(req.body);
+        let flightRequestData={
+            flightNumber: req.body.flightNumber,
+            airplaneId: req.body.airplaneId,
+            departureAirportId: req.body.departureAirportId,
+            arrivalAirportid: req.body.arrivalAirportid,
+            arrivalTime: req.body.arrivalTime,
+            departureTime: req.body.departureTime,
+            price: req.body.price
+        }
+        const flight= await flightService.createFlight(flightRequestData);
         return res.status(200).json({
             data: flight,
             success: true,
